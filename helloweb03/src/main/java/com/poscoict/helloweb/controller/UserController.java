1cf61f43f1718f6dd3f069fee76c879ca0cb62d3
BIT <BIT@DESKTOP-8FPA74D>
2022-01-27 15:25:59 +0900

    aop

--- helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
@@ -8,19 +8,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
- * @RequestMapping: 클래스(타입) + 메서드 (핸들러)
+ * @RequestMapping: 클래스(타입) + 메소드(핸들러)
  *
  */
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
-	@RequestMapping(value= "/join", method = RequestMethod.GET)
+
+	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String join() {
 		return "/WEB-INF/views/join.jsp";
 	}
-			
-	@RequestMapping(value= "/join", method = RequestMethod.POST)
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(UserVo vo) {
 		System.out.println(vo);
 		return "redirect:/";
@@ -30,21 +31,21 @@ public class UserController {
 	@RequestMapping("/update")
 	public String update(@RequestParam("n") String name) {
 		/**
-		 * 만일 , n이라는 이름의 파라미터가 없는 경우 
-		 * 400 bad request 에러가 발생한다.
+		 *  만일, n이라는 이름의 파라미터가 없는 경우
+		 *  400 bad request 에러가 발생한다.
 		 */
 		System.out.println(name);
-		return "UserController.update():";
+		return "UserController.update()";
 	}
 	
 	@ResponseBody
 	@RequestMapping("/update2")
 	public String update2(
-			@RequestParam(value = "n", required = true, defaultValue="") String name,
-			@RequestParam(value = "a", required = true, defaultValue="0") Integer age) {
-		System.out.println("----"+ name + "----");
-		System.out.println("----"+ age + "----");
-		return "UserController.update2():"+ name + age;
+			@RequestParam(value="n", required=true, defaultValue="") String name,
+			@RequestParam(value="a", required=true, defaultValue="0") Integer age) {
+		System.out.println("-----" + name + "-----" );
+		System.out.println("-----" + age + "-----" );
+		return "UserController.update2()";
 	}
 	
 }
