dbbd12c2433dfc33ab6345605b0a725002b96c5f
BIT <BIT@DESKTOP-8FPA74D>
2022-01-28 16:42:35 +0900

    tomcat 배포

--- emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
+++ emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
@@ -24,10 +24,10 @@ public class EmaillistRepository {
 		
 		try {
 			//1. JDBC 드라이버 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url ="jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC"; 
+			String url ="jdbc:mysql://192.168.0.57:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC"; 
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 				
 			//3. SQL 준비
@@ -129,10 +129,10 @@ public class EmaillistRepository {
 		
 		try {
 			//1. JDBC 드라이버 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url ="jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC"; 
+			String url ="jdbc:mysql://192.168.0.57:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC"; 
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		}catch(ClassNotFoundException e) {
 			System.out.print("드라이버 로딩 실패 : " + e);
